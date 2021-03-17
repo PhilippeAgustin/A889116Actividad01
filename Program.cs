0da03635659d52589e89aceef9a8f45f2250d0da
PhilippeAgustin <80849704+PhilippeAgustin@users.noreply.github.com>
2021-03-17 16:19:38 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A889116Actividad01
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Hola Mundo!");
+
+
+            Console.WriteLine("Presione enter para salir...");
+            Console.ReadLine();
+        }
+    }
+}
